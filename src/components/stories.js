bd1f1987e10f170bd594c505e99b404f05c34a50
Thiago Morales <thiago.morales@olist.com>
2021-09-30 20:28:48 -0300

    Adjust stories overflow.

--- src/components/stories.js
+++ src/components/stories.js
@@ -6,7 +6,7 @@ import StoryItem from './story-item';
 export default function Stories({ stories }) {
   return (
     <Box className="stories-container sm:full-width" border>
-      <div className="stories-feed overflow-y-hidden overflow-x-auto py-4 px-2 flex relative items-center">
+      <div className="stories-feed overflow-y-hidden overflow-x-scroll py-4 px-2 flex relative items-center">
         <div className="stories-feed-floating flex relative transition ease-linear duration-300">
           {stories &&
             stories.map((item) => {
---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
