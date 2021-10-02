6b285ab53f60e11d0dacf8ba98d347ccf78fbfb1
Thiago Morales <thiago.morales@olist.com>
2021-10-01 23:30:44 -0300

    Add missing js ext.

--- src/pages/[pid].tsx
+++ src/pages/[pid].tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useRouter } from 'next/router';
 
-import Layout from '../components/layout';
+import Layout from '../components/layout.js';
 
 const ProfilePage = observer(() => {
   const router = useRouter();
